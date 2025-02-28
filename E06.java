1efdef6fd7e9e278a1ac294aac12ea71e39d4f16
Manuelvilpas <162446744+Manuelvilpas@users.noreply.github.com>
2025-02-28 10:42:00 +0100

    fallo sistem

--- E06.java
+++ E06.java
@@ -3,7 +3,7 @@ public class E06{
 	public static void main(String[] args){
 		Scanner teclado=new Scanner(System.in);
 		float t;
-		System.out.println("Dame una temperatura en celsius");
+		Syste.out.println("Dame una temperatura en celsius");
 		t=teclado.nextFloat();
 		System.out.println(t+" en grados fahrenheit es "+(1.8*t+32));
 	}		
