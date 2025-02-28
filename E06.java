a9a3afdd4a52566ee6409691ed83dde490bbaae9
Manuelvilpas <162446744+Manuelvilpas@users.noreply.github.com>
2025-02-28 10:50:50 +0100

    arreglado

--- E06.java
+++ E06.java
@@ -3,7 +3,7 @@ public class E06{
 	public static void main(String[] args){
 		Scanner teclado=new Scanner(System.in);
 		float t;
-		Syste.out.println("Dame una temperatura en celsius");
+		System.out.println("Dame una temperatura en celsius");
 		t=teclado.nextFloat();
 		System.out.println(t+" en grados fahrenheit es "+(1.8*t+32));
 	}		
